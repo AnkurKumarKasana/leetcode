361eb03f098aed70122599f5cea89c7925e81b4a
Ankur  Kumar Kasana <119168505+AnkurKumarKasana@users.noreply.github.com>
2025-02-03 19:18:24 +0530

    Time: 30 ms (66.52%), Space: 61.1 MB (96.18%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,12 +3,14 @@ public:
     bool containsDuplicate(vector<int>& a) {
         if (a.empty()) return false;
         sort(a.begin(),a.end());
-
+// pehle sort karo jisse ki adjacent element ko compare kar sake
         for(int j=1;j<a.size();j++){
+// agar j or j-1 equal hai to array me duplicates elements hai to true return kardo 
             if(a[j]==a[j-1]){
                 return true;
             }
-        }return false;
+        }//nahi to false return kardo 
+        return false;
 
         
     }
