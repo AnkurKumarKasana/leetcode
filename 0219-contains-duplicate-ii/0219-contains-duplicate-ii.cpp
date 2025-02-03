0171d6240dc68019793738517fc93ad953a5d7f9
Ankur  Kumar Kasana <119168505+AnkurKumarKasana@users.noreply.github.com>
2025-02-03 19:52:34 +0530

    Time: 42 ms (99%), Space: 81.1 MB (66.51%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -3,8 +3,11 @@ class Solution {
     bool containsNearbyDuplicate(vector<int>& a, int k) {
         unordered_map <int,int> m;
         for(int i=0;i<a.size();i++){
+//yha check hora hai ki a[i] pehle se set me hai ya nahi 
+// agar hai to continue -- nahi to if se bhaar aajayega
             if(m.find(a[i])!=m.end()){
                 if (i - m[a[i]] <= k){
+// m[a[i]] this is j ---- yha i-j check karo agar <=k hai to true 
                     return true;
             }
         }m[a[i]]=i;
