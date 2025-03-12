78d6767fa865ef1d5275b247d4c3efa4045b89fc
Ankur  Kumar Kasana <119168505+AnkurKumarKasana@users.noreply.github.com>
2025-03-12 22:13:53 +0530

    Time: 86 ms (46.69%), Space: 88.9 MB (19.66%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> s(nums.begin(), nums.end());
-        int largest=0;
-        for(int x: s){
-            if(s.find(x-1)==s.end()){
-                int count =1;
+        unordered_set<int> s(nums.begin(),nums.end());
+        int ans=0;
+        for(int x:s){
+            if(s.find(x-1)==s.end()){/* for finding beginning number*/
+//upar wali line me ye check hoga ki x se chota set me hai kya aese karte karte karte wo set ki sbse choti value pe chla jayega or fir wha se if ke andar chla jayega 
+                int count=1;
                 while(s.find(x+count)!=s.end()){
                     count++;
-                } 
-                largest=max(count,largest);
+                }
+                ans=max(ans,count);
             }
-          }
-    return largest;
+        }return ans;
+        
     }
-}; 
+};
