f5a8bbf7f6954f4d934208cb30f2a9ebc436eba4
Ankur  Kumar Kasana <119168505+AnkurKumarKasana@users.noreply.github.com>
2025-03-20 20:06:38 +0530

    Time: 0 ms (100%), Space: 17.5 MB (86.66%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
+    
+    int findleft(vector<int>& arr, int target,int n){
+        int l=0,r=n-1;
+        int leftmost=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(arr[mid]==target){
+                leftmost=mid;
+                r=mid-1;
+            }else if(arr[mid]<target){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return leftmost;
+    }
+    int findright(vector<int>& arr, int target,int n){
+        int l=0,r=n-1;
+        int rightmost=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(arr[mid]==target){
+                rightmost=mid;
+                l=mid+1;
+            }else if(arr[mid]<target){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return rightmost;
+
+    }
     vector<int> searchRange(vector<int>& arr, int target) {
-        vector <int> result(2,-1);
-        int n=arr.size();
-        // for the first index of target
-        for(int i=0;i<n;i++){
-            if(arr[i]==target){
-                result[0]=i;
-                break;
-            }
-        }
-        // agar result array me abhi bhi same value hai to nahi mili
-        if(result[0]==-1)return result;
-        // agar pehla element milgya to dusra ke liye naya loop
-        for(int i=result[0];i<n;i++){
-            if(arr[i]==target){
-                result[1]=i;
-            }
-        }
-        return result ; 
+        int n= arr.size();
+        int leftmost= findleft(arr,target,n);
+        int rightmost=findright(arr,target,n);
+        return {leftmost,rightmost};
+        
     }
 };
