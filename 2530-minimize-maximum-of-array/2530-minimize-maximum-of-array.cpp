dc5bb71b41aec0eeff2bdb92a43f67ae9cdc8574
Ankur  Kumar Kasana <119168505+AnkurKumarKasana@users.noreply.github.com>
2025-03-26 19:42:33 +0530

    Time: 65 ms (19.2%), Space: 182.2 MB (22.96%) - LeetHub

--- 2530-minimize-maximum-of-array/2530-minimize-maximum-of-array.cpp
+++ 2530-minimize-maximum-of-array/2530-minimize-maximum-of-array.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    bool isValid(vector<int>& nums, int midmax,int n){
+    bool isValid(vector<int>& nums, int mid,int n){
         vector<long long>arr(begin(nums),end(nums));
         for(int i=0;i<n-1;i++){
-            if(arr[i]>midmax)
+            if(arr[i]>mid)
             return false;
 
-            long long buffer=midmax-arr[i];
+            long long buffer=mid-arr[i];
             arr[i+1]-=buffer;
         }
-        return arr[n-1]<=midmax;
+        return arr[n-1]<=mid;
     }
     int minimizeArrayValue(vector<int>& nums) {
         int n=nums.size();
-        int maxL=0;
-        int maxR= *max_element(begin(nums),end(nums));
+        int l=0;
+        int r= *max_element(begin(nums),end(nums));
         int result=0;
-        while(maxL<=maxR){
-            int midmax=maxL+(maxR-maxL)/2;
-            if(isValid(nums,midmax,n)){
-                result= midmax;
-                maxR=midmax-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(isValid(nums,mid,n)){
+                result= mid;
+                r=mid-1;
             }else{
-                maxL=midmax+1;
+                l=mid+1;
             }
         }
         return result;
