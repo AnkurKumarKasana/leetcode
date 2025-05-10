6120ca323421cf7be293062c15f494eff0a41e5a
Ankur  Kumar Kasana <119168505+AnkurKumarKasana@users.noreply.github.com>
2025-05-10 22:42:05 +0530

    Time: 0 ms (100%), Space: 8.6 MB (54.12%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,6 +3,9 @@ public:
     int reverse(int x) {
         int ans=0;
        while(x!=0){
+        if (ans > INT_MAX / 10 || ans < INT_MIN / 10) {
+                return 0; // Return 0 to indicate overflow or underflow
+            }
         int a=x%10;
         ans=(ans*10)+a;
         x/=10;
